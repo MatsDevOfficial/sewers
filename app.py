c5c831bb1e711efc99f4a73cd4b3c88e0cdecdda
Sebastian Cunningham <80603287+sebcun@users.noreply.github.com>
2025-12-16 15:18:10 +1100

    Added config for FAQs and title, as well as merged from https://github.com/sebcun/homelab

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
+import json
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    with open('config.json', 'r') as f:
+        config = json.load(f)
+    return render_template('index.html', config=config)
 
 if __name__ == '__main__':
     app.run(debug=True)
